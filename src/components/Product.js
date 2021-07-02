f082027b9abf60fe0764add4424e2b0773acbcf2
Christian Yungtay <christianyungtay@poli.ufrj.br>
2021-07-02 17:18:53 -0300

    merge with main and some fixes

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,36 +4,38 @@ import { useContext, useEffect, useState } from "react";
 import UserContext from "../context/UserContext";
 
 export default function Product({ item }) {
-     
-    return (
-        <Container>
-            <Title>{item.name} Quantidade: ({item.quantity}) </Title>                                       
-            <br />   
-            <Price>
-            Preço unitário: R${(item.price/100).toFixed(2).replace(".",",").replace("-","")}
-            </Price>         
-        </Container>
-    )
+  return (
+    <Container>
+      <Title>
+        {item.name} Quantidade: ({item.quantity}){" "}
+      </Title>
+      <br />
+      <Price>
+        Preço unitário: R$
+        {(item.price / 100).toFixed(2).replace(".", ",").replace("-", "")}
+      </Price>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: wrap;
+  display: flex;
+  flex-direction: wrap;
 `;
 
 const Title = styled.div`
-    margin-bottom: 20px;
-    font-size: 25px;
+  margin-bottom: 20px;
+  font-size: 25px;
 
-    @media (max-width: 640px) {           
-       font-size: 15px;    
-    }
+  @media (max-width: 640px) {
+    font-size: 15px;
+  }
 `;
 
 const Price = styled.div`
-    font-size: 25px;
+  font-size: 25px;
 
-    @media (max-width: 640px) {           
-       font-size: 15px;    
-    }
+  @media (max-width: 640px) {
+    font-size: 15px;
+  }
 `;
